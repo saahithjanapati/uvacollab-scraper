c31be94168270132194bcb6528e5d9aec5b0e17f
Saahith <22772542+saahithjanapati@users.noreply.github.com>
2022-05-26 12:27:57 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,15 +2,14 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-import urllib
-import requests
+
+from urllib.parse import unquote
 import time
-
-class Folder:
-    def __init__(self, name, parent=None, children=[]):
-        self.parent = parent
-        self.children = children
+import os
 
 
 class Scraper:
@@ -23,98 +22,137 @@ class Scraper:
                 "download.prompt_for_download": False,
                 "download.directory_upgrade": True,
                 "plugins.always_open_pdf_externally":True}
+
         options.add_experimental_option('prefs', prefs)
-        
-        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         self.ac = ActionChains(self.driver)
+        self.num_files_to_download = 0
 
+        if not os.path.exists(self.download_base_dir):
+            os.makedirs(self.download_base_dir)
 
-    # def download_documents_on_page(self, class_names=['fa-file-pdf-o', 'fa-file-ppt-o']):
-    #     """downloads all documents with given class on page (default document type is pdfs, but can change to powerpoint by changing class_name to fa-file-powerpoint-o"""
-    #     job_list = []
-    #     names = []
-    #     for class_name in class_names:
-    #         job_list = job_list + self.driver.find_elements_by_class_name(class_name)
-    #     for job in job_list:
-    #         names.append(urllib.parse.unquote(job.get_attribute('href').split("/")[-1]))
-    #         job.click()
-    #     return names
-    
 
     def open_folders(self):
         """opens all folders (and subfolders) on screen"""
-        # while(job_list)
         job_list = self.driver.find_elements_by_class_name("fa-folder")
         opened = 0
+        print("Opening all folders")
         while(len(job_list) > 0):
             job = job_list[0]
-            # input("next step")
-            
             time.sleep(1.5)
-            self.ac.move_to_element(job).move_by_offset(-100, 0).click().perform()
-            print("opening ", job.get_attribute('href'))
             try:
                 job.click()
                 opened += 1
-
             except:
-                print("retrying")
-                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-            # print("opened ", job.get_attribute('href'))
+                self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
             job_list = self.driver.find_elements_by_class_name("fa-folder")
-    
 
 
-    def scrape(self):
+
+    def start_download(self):
         """does the scraping"""
         job_list = []
-        folder_stack = []
+        class_names = ['fa-file-pdf-o', 'fa-file-powerpoint-o', 'fa-file-word-o']
+
+        for class_name in class_names:
+            # job_list += self.driver.find_elements_by_class_name(class_name)
+            job_list += self.driver.find_elements_by_class_name(class_name)
+            # job_list += self.driver.find_elements(by=By.CLASS_NAME, value=class_name)
+        i = 0
+        while i < len(job_list):
+            job = job_list[i]
+            try:
+                job.click()
+                i += 1
+            except:
+                self.driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+
+
+
+    def wait_for_download_to_complete(self, structure):
+        downloaded_files = [f for f in os.listdir(self.download_base_dir) if f.endswith('.pdf') or f.endswith('.ppt') or f.endswith('.pptx') or f.endswith(".docx")]
+        while len(downloaded_files) != self.num_files_to_download:
+            print("downloaded files: ", len(downloaded_files))
+            print("number of files needed: ", self.num_files_to_download)
+            print("")
+            time.sleep(1)
+            downloaded_files = [f for f in os.listdir(self.download_base_dir) if f.endswith('.pdf') or f.endswith('.ppt') or f.endswith('.pptx') or f.endswith(".docx")]
+        return
+
+
+    def get_file_structure(self):
+        """determines file/folder structure so we can can replicate that structure in the downloaded folders"""
+        """returns sorted array containing info from wich file structure can be generated"""
+        print("Determining folder structure...")
+        job_list = []
         class_names = ['fa-file-pdf-o', 'fa-file-powerpoint-o', 'fa-file-word-o', 'fa-folder-open']
         for class_name in class_names:
+            # job_list += self.driver.find_elements_by_class_name(class_name)
             job_list += self.driver.find_elements_by_class_name(class_name)
-        job_list = sorted(job_list, key=lambda job: job.location['y'])
-        for job in job_list:
-            print(job.get_attribute("href"), job.location['y'])
-        
-        print("\n"*5)
 
+
+        job_list = sorted(job_list, key=lambda job: job.location['y'])
+        # name_elements = self.driver.find_elements_by_class_name("resource-name")
+        # name_elements = self.driver.find_elements(by=By.CLASS_NAME, value="resource-name")
         name_elements = self.driver.find_elements_by_class_name("resource-name")
-        names = []
+
+
+        names, structure = [], []
         for elem in name_elements:
-            # print(elem.text, elem.location['y'])
             names.append((elem.text, elem.location['y']))
-        
-        
+
         for job in job_list:
             for name, y in names:
                 if job.location['y'] == y+3:
-                    print(job.get_attribute("class"), name, job.location['x'])
+                    actual_name = name
+                    if job.get_attribute("class") != 'nil fa fa-folder-open':   # if it's not a folder, get the actual name from the href
+                        actual_name = unquote(job.get_attribute('href').split("/")[-1])
+                    structure.append((job.get_attribute("class"), actual_name, job.location['x']))
                     break
 
+        for elem in structure:
+            print(elem)
+            if elem[0] == 'fa fa-file-pdf-o' or elem[0]=='fa fa-file-powerpoint-o' or elem[0]=='fa fa-file-word-o':
+                self.num_files_to_download += 1
 
+        return structure
+
+
+    def organize_files(self, structure):
+        """reorganizes files once they are all downloaded into folder structure present on collab site"""
+        structure = structure[1:]
+        # curr_dir = self.download_base_dir
+        stack = [(0, self.download_base_dir)]    # stack: [(x_pos, path)]
+        # obj: (class_name, name, x_pos)
+        for obj in structure:
+            class_name, name, x_pos = obj[0], obj[1], obj[2]
+            while stack[-1][0] >= x_pos:
+                stack.pop()
+
+            if class_name == 'nil fa fa-folder-open':   # it's a folder
+                new_path = os.path.join(stack[-1][1], name)
+                os.mkdir(new_path)
+                stack.append((x_pos, new_path))
+
+            else:   # it's a file, need to save to current directory
+                # save file to current path
+                curr_dir = os.path.join(self.download_base_dir, name)
+                new_dir = os.path.join(stack[-1][1], name)
+                os.replace(curr_dir, new_dir)
 
 
 def main():
-    BASE_DIR = "/Users/saahith/Desktop/uvacollab-scraper/test/"
+    BASE_DIR = input("Enter the path to an empty directory to which you want to download the files to: ")
     my_scraper = Scraper(BASE_DIR)
 
     done = input("enter something when you have navigated to the page")
     my_scraper.open_folders()
-    my_scraper.scrape()
+    structure = my_scraper.get_file_structure()
+    my_scraper.start_download()
+    print(structure)
+    my_scraper.wait_for_download_to_complete(structure)
+    my_scraper.organize_files(structure)
 
-
-    # print("waiting time is done")
-    # links_with_names = my_scraper.download_documents_on_page()
-    # done = input("terminate the program once everything is finished downloaded")
-
-    
-    
-    # for link, name in links_with_names:
-    #     my_scraper.download_file_to_dir(link, base_dir+name)
-    
-
-    # print(list(links_with_names))
 
 if __name__ == "__main__":
     main()
